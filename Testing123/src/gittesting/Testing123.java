b05469e94907f29a657f4bee4d1d01ea3332255a
Stoze <stoze1337@gmail.com>
2013-03-29 23:46:57 -0700

    Commit2

--- Testing123/src/gittesting/Testing123.java
+++ Testing123/src/gittesting/Testing123.java
@@ -2,12 +2,8 @@ package gittesting;
 
 public class Testing123 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Dis is a test for git.");
 	}
 
 }
