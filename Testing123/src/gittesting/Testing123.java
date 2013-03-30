6902bf8f736d3e2895b69e45b6ee2725f1ef5507
Dwatts92 <ddwatts@asu.edu>
2013-03-30 10:48:16 -0700

    Update Testing123.java
    another commit

--- Testing123/src/gittesting/Testing123.java
+++ Testing123/src/gittesting/Testing123.java
@@ -5,6 +5,7 @@ public class Testing123 {
 	public static void main(String[] args) {
 		System.out.println("Testing testing...");
 		System.out.println("blasdfhasdfsf");
+		System.out.println("hey hey hey");
 
 	}
 
