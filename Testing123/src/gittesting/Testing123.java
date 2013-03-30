21964358fd72e097baaf275879a75d589effca70
Stoze <stoze1337@gmail.com>
2013-03-30 01:10:17 -0700

    Another commit

--- Testing123/src/gittesting/Testing123.java
+++ Testing123/src/gittesting/Testing123.java
@@ -3,8 +3,7 @@ package gittesting;
 public class Testing123 {
 
 	public static void main(String[] args) {
-		System.out.println("Dis is a test for git.");
-		System.out.println("Trying another commit.");
+		System.out.println("Testing testing...");
 	}
 
 }
