dfa01ec7040b23802691e0a327cd68e8c86becbf
Dwatts92 <ddwatts@asu.edu>
2013-03-30 08:13:33 -0700

    Update Testing123.java
    More changes

--- Testing123/src/gittesting/Testing123.java
+++ Testing123/src/gittesting/Testing123.java
@@ -4,9 +4,7 @@ public class Testing123 {
 
 	public static void main(String[] args) {
 		System.out.println("Testing testing...");
-		System.out.println("blah blah.");
-		System.out.println("dadadooda");
-		
+
 	}
 
 }
